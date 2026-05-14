11633521032e79d727af0a99be7ee81f332c9bb1
NetFeez <netfeez.dev@gmail.com>
2026-05-14 06:15:06 +0000

    chore: renamed folder Time to time

--- src/common.ts
+++ src/common.ts
@@ -1,5 +1,5 @@
 export { Schema } from './schema/index.js';
-export { Time } from './Time/index.js';
+export { Time } from './time/index.js';
 export { Object } from './Object.js';
 export { Events } from './Events.js';
 export { Flatten } from './Flatten.js';
